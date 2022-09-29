56f3db993c1c6ac13d32d588e554eeae8251db6a
thorgit <youssefm@telefonica.net>
2022-09-29 20:50:59 +0200

    ya me he perdido

--- digitalClock.py
+++ digitalClock.py
@@ -1,5 +1,8 @@
+#VENTANA CON CUADRO CON RELOJ O VENTANA DE WELCOME
+
+
 from tkinter import Tk, Label
-from datetime import datetime
+#from datetime import datetime
 
 window = Tk()
 window.title("Digital Clock")
@@ -9,11 +12,11 @@ window.configure(bg="red")
 label = Label(window, text="Welcome!",font=("Arial Black",78,"bold"), bg="red", fg="white")
 label.pack(pady=100)
 
-def clock():
-    time=datetime.now().strftime("%H:%M:%S")
-    label.configure(text=time)
-    label.after(500,clock)
+#def clock():
+    #time=datetime.now().strftime("%H:%M:%S")
+    #label.configure(text=time)
+    #label.after(500,clock)
 
-clock()
+#clock()
 window.mainloop()
 
